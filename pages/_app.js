4b5067de850ca46826d0839e5eaa32c8c969a38e
Usmaarn <usmaarn@gmail.com>
2022-07-28 04:33:52 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,16 @@
-import Navbar from '@/components/Navbar';
-import {AppContext} from '@/utils/appContext';
-import { Component } from 'react';
-import 'styles/globals.css';
+/** @format */
 
+import { AppContext } from "@/utils/appContext";
+import "styles/globals.css";
 
-class MyApp extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      darkMode: true,
-    }
-  }
-
-  toggleTheme = () => this.setState({darkMode: !this.state.darkMode})
-
-  render() {
-
-    const { Component, pageProps } = this.props;
-    const appClassName = this.state.darkMode ? 'dark' : '';
-    const contexts = {...this.state, 
-      toggleTheme: this.toggleTheme
-    }
-
-    return(
-      <AppContext.Provider value={contexts}>
-        <div className={appClassName}>
-          <div className='min-h-screen dark:bg-gray-900 dark:text-white transition duration-500'>
-            {/* <Navbar /> */}
-            <Component {...pageProps} />
-          </div>
+export default function MyApp() {
+  return (
+    <AppContext.Provider>
+      <div cclassName="bg-[#2D2A23  ]">
+        <div className="">
+          <Component {...pageProps} />
         </div>
-      </AppContext.Provider>
-    )
-  }
+      </div>
+    </AppContext.Provider>
+  );
 }
-
-export default MyApp
