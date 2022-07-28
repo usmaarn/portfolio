f8dda36678aac971b9d739732293e3b78b6cd681
Usmaarn <usmaarn@gmail.com>
2022-07-28 14:38:07 +0100

    added social media links

--- layouts/Footer.jsx
+++ layouts/Footer.jsx
@@ -2,7 +2,7 @@
 
 export default function Footer(){
     return(
-        <div className="mt-20 py-5 bg-slate-300 text-center capitalize font-semibold tracking-wide">
+        <div className="mt-20 py-5 bg-black/90 text-gray-400 text-center capitalize font-semibold tracking-wide">
             <p className='flex items-center gap-2 justify-center'>
                 <span>
                     &copy; {new Date().getFullYear()} <a href="/">Dev Usman</a>
