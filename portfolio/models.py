e1dfa2c2e610b9a848c3ff7530a91fdc20bb6d47
usmaarn <usmaarn@gmail.com>
2024-03-27 18:14:23 +0100

    2024-03-27 18:14:23.176563 - 0fd0339b-2817-43fb-bb3e-5c2c5d3e7042

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,9 +1,29 @@
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
+from typing import List
 
 from . import db
 
 class User(db.models):
+    __tablename__ = 'users'
+
     id: Mapped[int] = mapped_column(primary_key=True)
-    firstname: Mapped[int] = mapped_column(nullable=False)
-    lastname: Mapped[int] = mapped_column(nullable=)
+    firstname: Mapped[str] = mapped_column(nullable=False)
+    lastname: Mapped[str] = mapped_column(nullable=False)
+    username: Mapped[str] = mapped_column(unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    created_at: Mapped[DateTime] = mapped_column()
+    email_verified_at: Mapped[DateTime] = mapped_column()
+
+    def __repr__(self) -> str:
+        pass
+
+
+class Subscriber(db.models):
+    __tablename__ = 'subscribers'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    created_at: Mapped[DateTime] = mapped_column()
