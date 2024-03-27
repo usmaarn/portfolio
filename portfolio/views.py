3fd4d02b2a669a50648b800423d5968c92bb8613
usmaarn <usmaarn@gmail.com>
2024-03-27 15:19:34 +0100

    2024-03-27 15:19:34.083014 - 82102ad8-7742-4eae-a3de-08aca28895c9

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,15 +1,16 @@
 from flask import render_template, redirect, request, Blueprint
 
-
 bp = Blueprint('app', __name__)
 
 @bp.route('/')
 def home():
     return render_template('home.html')
 
- 
-def loginPage():
-    pass
+
+@bp.route('/login', methods=['POST', 'GET'])
+def login():
+    return render_template('login.html')
+
 
 def subscribe():
     pass
